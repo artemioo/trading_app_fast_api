3245569b27d1111d2108196593044f0d740dcb63
artemioo <artemryb1@gmail.com>
2023-07-14 11:59:39 +0300

    some updates

--- main.py
+++ main.py
@@ -40,16 +40,6 @@ app.include_router(
 )
 
 
-fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-    {"id": 4, "role": "investor", "name": "Homer", "degree": [
-        {'id': 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
-    ]},
-]
-
-
 class DegreeType(Enum):
     newbie = 'newbie'
     expert = 'expert'
@@ -68,17 +58,6 @@ class User(BaseModel):
     degree: Optional[List[Degree]]
 
 
-@app.get('/users/{user_id}', response_model=List[User])
-async def hello(user_id: int):
-    return [user for user in fake_users if user.get("id") == user_id]
-
-
-fake_trades = [
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": 'buy', "price": 123, "amount": 2.12},
-    {"id": 2, "user_id": 1, "currency": "BTC", "side": 'sell', "price": 125, "amount": 2.12},
-]
-
-
 class Trade(BaseModel):
     id: int
     user_id: int
@@ -87,21 +66,3 @@ class Trade(BaseModel):
     price: float = Field(ge=0)
     amount: float
 
-
-@app.post('/trades')
-async def add_trades(trades: List[Trade]):
-    fake_trades.extend(trades)
-    return {'status': 200, 'data': fake_trades}
-
-fake_users2 = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-]
-
-
-@app.post('/users/{users_id}')
-async def change_name(user_id: int, new_name: str):
-    user = list(filter(lambda user: user.get('id') == user_id, fake_users2))[0]
-    user['name'] = new_name
-    return {"status": 200, 'data': user}
